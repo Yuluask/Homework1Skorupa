edb2dd8fbe0fa280271d9a08bc15d8e6de1b5652
Yuluask <7091062@mail.ru>
2023-10-30 20:18:57 +0300

    add square

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        square(5);
     }
+
+    public static void square (int a)
+    {
+        System.out.printf( "Площадь квадрата равна %d", a*a );
+    }
+
+
+
 }
+
+
+
